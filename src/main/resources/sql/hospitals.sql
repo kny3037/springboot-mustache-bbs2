fc3287eec801fac8cb094a0345af77cc6a8db329
psi5059 <psi5059@naver.com>
2022-11-14 14:22:02 +0900

    특정 구의 - 보건진료소, 보건지소, 보건소 모두 찾기

--- src/main/resources/sql/hospitals.sql
+++ src/main/resources/sql/hospitals.sql
@@ -7,5 +7,5 @@ select hospital_name, full_address from `likelion-db1`.nation_wide_hospitals whe
 -- 특정 구의 - 보건진료소, 보건지소, 보건소 모두 찾기
 select business_type_name, hospital_name, road_name_address
 from `likelion-db1`.nation_wide_hospitals
-where business_type_name in ('보건소', '보건지소')
+where business_type_name in ('보건소', '보건지소', '보건진료소')
 ;
