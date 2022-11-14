05151dfda96a8e62e6e6522c917e7e94ea90fb83
psi5059 <psi5059@naver.com>
2022-11-14 14:22:40 +0900

    businessTypeName 추가

--- src/main/java/com/mustache/bbs2/domain/entity/Hospital.java
+++ src/main/java/com/mustache/bbs2/domain/entity/Hospital.java
@@ -22,6 +22,7 @@ public class Hospital {
     private String hospitalName;
     private Integer patientRoomCount;
     private Integer totalNumberOfBeds;
+    private String businessTypeName;
     private Float totalAreaSize;
 
 
