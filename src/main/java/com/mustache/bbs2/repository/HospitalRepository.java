e6520b90471be906cdf83c8c14c94d73a77e9161
psi5059 <psi5059@naver.com>
2022-11-14 14:22:20 +0900

    findByBusinessTypeNameIn

--- src/main/java/com/mustache/bbs2/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs2/repository/HospitalRepository.java
@@ -3,5 +3,11 @@ package com.mustache.bbs2.repository;
 import com.mustache.bbs2.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+
+
+
 }
