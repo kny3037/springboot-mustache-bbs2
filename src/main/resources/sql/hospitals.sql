96d372907e87610713e5a605d7bbe6b2a8efa33d
psi5059 <psi5059@naver.com>
2022-11-14 13:26:20 +0900

    특정 조건 검색 sql like

--- src/main/resources/sql/hospitals.sql
+++ src/main/resources/sql/hospitals.sql
@@ -2,4 +2,4 @@ SELECT * FROM `likelion-db1`.nation_wide_hospitals;
 
 -- 경기도 수원시에 있는 피부과를 모두 찾아보세요
 
-select full_address from `likelion-db1`.nation_wide_hospitals where full_address like '경기도 수원시%';
+select hospital_name, full_address from `likelion-db1`.nation_wide_hospitals where full_address like '경기도 수원시%';
